90ba6b1df1066cadd7bed84e1ce6e5b3cf2a74db
Shreya Palase <59469602+shreyapalase@users.noreply.github.com>
2026-06-12 15:44:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    result = ""
-
-    if request.method == "POST":
-        temperature = int(request.form["temperature"])
-
-        if temperature >= 35:
-            result = "Sunny ☀️"
-        elif temperature >= 25:
-            result = "Partly Cloudy ⛅"
-        elif temperature >= 15:
-            result = "Cloudy ☁️"
-        else:
-            result = "Rainy 🌧️"
-
-    return render_template("index.html", result=result)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
